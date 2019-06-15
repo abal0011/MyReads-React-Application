4dda66636b2cebe495f61df408c051051f6e0fbf
abal0011 <aravind.cyber@yahoo.co.in>
2019-06-15 16:41:31 +1000

    search page finished

--- src/search.js
+++ src/search.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookShelf'
 
 export class Search extends Component {
+    state = { books: [] }
+
+    searchQuery = (event) => {
+        const query = event.target.value
+        if (query !== '') {
+            BooksAPI.search(query, 20).then(books => {
+                if (!books || books.error) {
+                    this.setState({ books: [] })
+
+                }
+                else {
+                    this.setState({ books })
+                }
+            })
+        }
+        else {
+            this.setState({ books: [] })
+        }
+
+    }
+
+    updateShelf = (book, shelf) => {
+        console.log(book, shelf)
+        BooksAPI.update(book, shelf).then(
+            this.setState(oldState => ({
+                books: oldState.books.map(b => {
+                    if (b.id === book.id) {
+                        b.shelf = shelf;
+                    }
+                    return b;
+                })
+            })
+            )
+        )
+    }
     render() {
         return (
             <div className="search-books">
@@ -18,12 +55,14 @@ export class Search extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                        <input type="text" placeholder="Search by title or author" />
+                        <input type="text" placeholder="Search by title or author" value={this.state.searchBook} onChange={this.searchQuery} />
 
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    {this.state.book = !undefined &&
+                        <BookShelf books={this.state.books} />
+                    }
                 </div>
             </div>
         )
